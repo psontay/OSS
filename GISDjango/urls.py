8a55c999bb4d0a5b4bb1f34ddaf287644965154d
nlt091105 <thinhvaba@gmail.com>
2026-01-29 20:28:32 +0700

    tao giao dien co ban

--- GISDjango/urls.py
+++ GISDjango/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for GISDjango project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    # Đã đổi sang /products/ cho ngắn gọn
+    path('products/', views.product_list, name='product_list'), 
+    # Trang giao diện bản đồ
+    path('check-suitability/', views.suitability_page, name='check_suitability'), 
+    # API xử lý dữ liệu PostGIS (gọi bằng fetch trong JS)
+    path('api/check/', views.check_suitability, name='api_check_suitability'), 
 ]
