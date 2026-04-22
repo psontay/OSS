c5bb16669b9d0cae044d95a7bcb3365c2aa5a08c
psontay <phamsontay2206@gmail.com>
2026-04-22 07:33:13 +0700

    feat: init model

---
+++ OSS/model/__init__.py
@@ -0,0 +1,6 @@
+from .order import Order
+from .order import OrderItem
+from .user import User
+from .plant import Plant
+from .store_branch import StoreBranch
+from .store_stock import StoreStock
--- OSS/model/forms.py
+++ OSS/model/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.db.models import Sum
 
-from GISDjango.models import User, StoreStock
+from OSS.model import User, StoreStock
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
